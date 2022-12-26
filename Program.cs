72e245956627fb5f34f836fb5ea69f16e843c33b
ShrineFox <ShrineFox@users.noreply.github.com>
2022-12-25 23:15:21 -0500

    (de)serialize uncompressed ACNH RSTC & dump to txt

---
+++ Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using DamienG.Security.Cryptography;
+
+namespace RSTBPatcher
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Reading Resource Table data...");
+
+            RSTB rstb = RSTB.Load(".\\2.0.6\\ResourceSizeTable.rsizetable");
+
+            Console.WriteLine($"Header Magic: {Encoding.ASCII.GetString(rstb.Header.Magic)}");
+            Console.WriteLine($"CRC32 Table Entries: {rstb.Header.EntryTableSize}");
+            Console.WriteLine($"Named Table Entries: {rstb.Header.NameTableSize}\n");
+
+            Console.WriteLine("Saving Resource Table data...");
+
+            RSTB.Save(rstb, "test.rstc");
+            Console.WriteLine($"Saved: test.rstc");
+
+            DumpRSTBTxt(rstb);
+        }
+
+        public static void DumpRSTBTxt(RSTB rstb, string outPath = ".\\ResourceSizeTableDump.txt")
+        {
+            Console.WriteLine("Writing txt...");
+
+            // Header Magic
+            string txt = $"{Encoding.ASCII.GetString(rstb.Header.Magic)}\n";
+            // CRC32 Table Entries
+            foreach (var entry in rstb.EntryTable)
+                txt += $"{CRC32ToHexString(entry.Crc32)} {entry.Size} {entry.Unknown}\n";
+            txt += "\n";
+            // Named Table Entries
+            foreach (var entry in rstb.NameTable)
+                txt += $"{NameEntryToString(entry.Name)} {entry.Size} {entry.Unknown}\n";
+            txt = txt.TrimEnd('\n');
+            File.WriteAllText(outPath, txt);
+            Console.WriteLine($"Done writing txt: {outPath}");
+        }
+
+        private static string NameEntryToString(byte[] name)
+        {
+            return Encoding.ASCII.GetString(name).TrimEnd('\0');
+        }
+
+        private static string CRC32ToHexString(uint crc32)
+        {
+            return BitConverter.ToString(BitConverter.GetBytes(crc32).Reverse().ToArray()).Replace("-", "");
+        }
+
+        private static uint HexStringToCRC32(string hexString)
+        {
+            // https://stackoverflow.com/questions/321370/how-can-i-convert-a-hex-string-to-a-byte-array#comment112899143_321370
+            byte[] data = BigInteger.Parse("00" + hexString, System.Globalization.NumberStyles.HexNumber).ToByteArray();
+            while (data.Count() < 4)
+                data = data.Concat(new byte[] { 0x00 }).ToArray();
+            return BitConverter.ToUInt32(data);
+        }
+
+        public static RSTB ReadRSTBTxt(string path = ".\\2.0.6\\ResourceSizeTable.rsizetable.txt")
+        {
+            // Load RSTB data from .txt dump
+            Console.WriteLine($"Reading txt: {path}");
+            RSTB rstb = new RSTB();
+
+            var lines = File.ReadAllLines(path);
+            bool namedTable = false;
+            // Start from line 2 since line 1 is reserved for header magic
+            for (int i = 1; i < lines.Count(); i++)
+            {
+                string[] splitLine = lines[i].Split(' ');
+
+                if (splitLine.Count() == 3)
+                {
+                    if (!namedTable)
+                        rstb.EntryTable.Add(new RSTBTableEntry(HexStringToCRC32(splitLine[0]), Convert.ToUInt32(splitLine[1]), Convert.ToUInt32(splitLine[2])));
+                    else
+                        rstb.NameTable.Add(new RSTBNameEntry(splitLine[0], Convert.ToUInt32(splitLine[1]), Convert.ToUInt32(splitLine[2])));
+                }
+                else
+                {
+                    // Switch to named table entries
+                    namedTable = true;
+                }
+            }
+            
+            // Create header
+            if (lines[0] == "RSTC")
+                rstb.Header.Magic = new byte[4] { 0x52, 0x53, 0x54, 0x43 };
+            rstb.Header.EntryTableSize = Convert.ToUInt32(rstb.EntryTable.Count);
+            rstb.Header.NameTableSize = Convert.ToUInt32(rstb.NameTable.Count);
+
+            Console.WriteLine("Done reading txt");
+            return rstb;
+        }
+    }
+}
---
+++ RSTB.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace RSTBPatcher
+{
+    public class RSTB
+    {
+        public RSTB() { }
+        public RSTB(RSTBHeader header, List<RSTBTableEntry> entryTable, List<RSTBNameEntry> nameTable)
+        {
+            Header = header;
+            EntryTable = entryTable;
+            NameTable = nameTable;
+        }
+        public RSTBHeader Header { get; set; } = new RSTBHeader();
+        public List<RSTBTableEntry> EntryTable { get; set; } = new List<RSTBTableEntry>();
+        public List<RSTBNameEntry> NameTable { get; set; } = new List<RSTBNameEntry>();
+        public static RSTB Load(string path)
+        {
+            RSTB rstb = new RSTB();
+            using (FileStream stream = new FileStream(path, FileMode.Open))
+            {
+                using (BinaryReader reader = new BinaryReader(stream))
+                {
+                    // Read header
+                    byte[] magic = reader.ReadBytes(4);
+                    if (magic.SequenceEqual(new byte[4] { 0x52, 0x53, 0x54, 0x42 }) || magic.SequenceEqual(new byte[4] { 0x52, 0x53, 0x54, 0x43 }))
+                        rstb.Header = new RSTBHeader(magic, reader.ReadUInt32(), reader.ReadUInt32());
+                    else
+                        throw new Exception("Invalid header. Must start with RSTB or RSTC.");
+
+                    // Read table entries
+                    List<RSTBTableEntry> entryTable = new List<RSTBTableEntry>();
+                    for (int i = 0; i < (int)rstb.Header.EntryTableSize; i++)
+                    {
+                        if (magic.SequenceEqual(new byte[4] { 0x52, 0x53, 0x54, 0x43 })) // RSTC
+                            entryTable.Add(new RSTBTableEntry(reader.ReadUInt32(), reader.ReadUInt32(), reader.ReadUInt32()));
+                        else // RSTB
+                            entryTable.Add(new RSTBTableEntry(reader.ReadUInt32(), reader.ReadUInt32()));
+                    }
+                    rstb.EntryTable = entryTable;
+
+                    // Read name entries
+                    List<RSTBNameEntry> nameTable = new List<RSTBNameEntry>();
+                    for (int i = 0; i < (int)rstb.Header.NameTableSize; i++)
+                    {
+                        if (magic.SequenceEqual(new byte[4] { 0x52, 0x53, 0x54, 0x43 })) // RSTC
+                            nameTable.Add(new RSTBNameEntry(Encoding.ASCII.GetString(reader.ReadBytes(128)), reader.ReadUInt32(), reader.ReadUInt32()));
+                        else // RSTB
+                            nameTable.Add(new RSTBNameEntry(Encoding.ASCII.GetString(reader.ReadBytes(128)), reader.ReadUInt32()));
+                    }
+                    rstb.NameTable = nameTable;
+                }
+            }
+            return rstb;
+        }
+        public static void Save(RSTB rstb, string outPath)
+        {
+            using (FileStream stream = new FileStream(outPath, FileMode.OpenOrCreate))
+            {
+                using (BinaryWriter writer = new BinaryWriter(stream))
+                {
+                    // Write Header
+                    writer.Write(rstb.Header.Magic);
+                    writer.Write(rstb.Header.EntryTableSize);
+                    writer.Write(rstb.Header.NameTableSize);
+                    // Write Table Entries
+                    foreach (var entry in rstb.EntryTable)
+                    {
+                        writer.Write(entry.Crc32);
+                        writer.Write(entry.Size);
+                        if (rstb.Header.Magic.SequenceEqual(new byte[4] { 0x52, 0x53, 0x54, 0x43 })) // RSTC
+                            writer.Write(entry.Unknown); // 4 bytes of padding
+                    }
+                    // Write Name Entries
+                    foreach (var entry in rstb.NameTable)
+                    {
+                        writer.Write(entry.Name);
+                        writer.Write(entry.Size);
+                        if (rstb.Header.Magic.SequenceEqual(new byte[4] { 0x52, 0x53, 0x54, 0x43 })) // RSTC
+                            writer.Write(entry.Unknown); // 4 bytes of padding
+                    }
+                }
+            }   
+        }
+    }
+
+    public class RSTBHeader
+    {
+        public RSTBHeader() { }
+        public RSTBHeader(byte[] magic, uint entryTableSize, uint nameTableSize)
+        {
+            Magic = magic;
+            EntryTableSize = entryTableSize;
+            NameTableSize = nameTableSize;
+        }
+        public byte[] Magic { get; set; } = new byte[4] { 0x52, 0x53, 0x54, 0x42 };
+        public uint EntryTableSize { get; set; } = 0;
+        public uint NameTableSize { get; set; } = 0;
+    } // sizeof() = 12
+
+    public class RSTBTableEntry
+    {
+        public RSTBTableEntry() { }
+        public RSTBTableEntry(uint crc32, uint size, uint unknown = 0)
+        {
+            Crc32 = crc32;
+            Size = size;
+            Unknown = unknown;
+        }
+        public uint Crc32 { get; set; } = 0;
+        public uint Size { get; set; } = 0;
+        public uint Unknown { get; set; } = 0; // Only used in RSTC
+    } // sizeof() = 8 (12)
+
+    public class RSTBNameEntry
+    {
+        public RSTBNameEntry() { }
+        public RSTBNameEntry(string name, uint size, uint unknown = 0)
+        {
+            Name = Encoding.ASCII.GetBytes(name).Concat(new byte[128 - Encoding.ASCII.GetBytes(name).Length]).Take(128).ToArray();
+            Size = size;
+            Unknown = unknown;
+        }
+        public byte[] Name { get; set; } = new byte[128];
+        public uint Size { get; set; } = 0;
+        public uint Unknown { get; set; } = 0; // Only used in RSTC
+    } // sizeof() = 132 (136)
+}
